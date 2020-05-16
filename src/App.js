644a986a4a6a7fc9d2d7931063947d298d8819e0
Thomas Ngo <tngo0508@gmai.com>
2020-05-15 17:28:56 -0700

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact name="John Doe" email="jdoe@gmail.com"
-          phone="777-777-7777"
-        />
-        <Contact name="Karen Smith" email="ksmith@gmail.com"
-          phone="777-777-7123"
-        />
+        <Header branding="Contact Mananger" />
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com"
+            phone="777-777-7777"
+          />
+          <Contact name="Karen Smith" email="ksmith@gmail.com"
+            phone="777-777-7123"
+          />
+        </div>
       </div >
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,20 @@ import PropTypes from 'prop-types';
 const Header = (props) => {
   const { branding } = props;
   return (
-    <div>
-      <h1 style={headingStyle}>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger">
+      <div className="container">
+        <a href="/" className="navbar-brand">{branding}</a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
@@ -17,8 +28,5 @@ Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
 
-const headingStyle = {
-  color: 'red', fontSize: '50px'
-}
 
 export default Header;
