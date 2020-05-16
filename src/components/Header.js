42f2b4524f96ce04676a2c6dba8e9e145225a219
Thomas Ngo <tngo0508@gmai.com>
2020-05-15 17:15:10 -0700

    work with style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   const { branding } = props;
   return (
     <div>
-      <h1>{branding}</h1>
+      <h1 style={headingStyle}>{branding}</h1>
     </div>
   );
 };
@@ -16,4 +16,9 @@ Header.defaultProps = {
 Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
+
+const headingStyle = {
+  color: 'red', fontSize: '50px'
+}
+
 export default Header;
