8025bf5e381f8e8289faf3d3c1d46cb16336c28f
Thomas Ngo <tngo0508@gmai.com>
2020-05-15 17:09:05 -0700

    work with Props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  // static propTypes = {
+  //   name: PropTypes.string.isRequired,
+  //   email: PropTypes.string.isRequired,
+  //   phone: PropTypes.string.isRequired,
+  // }
   render() {
     const { name, email, phone } = this.props;
     return (
       <div>
         <h4>{name}</h4>
         <ul>
-          <li>{email}</li>
-          <li>{phone}</li>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
         </ul>
       </div>
     )
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+};
 
 export default Contact
