d6ba874cb598be767791eec27dbcc9d569bc10ed
Thomas Ngo <tngo0508@gmai.com>
2020-05-17 14:04:26 -0700

    learning events in React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {};
+
+  //   this.onShowClick = this.onShowClick.bind(this);
+  // }
+  state = {};
+  onShowClick = (id, e) => {
+    console.log(id);
+  }
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>{name} <i onClick={this.onShowClick.bind(this, name)} className="fas fa-sort-down"></i></h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
