3371b6337c04bb3d4c95fda6b9cad668814cffa8
Thomas Ngo <tngo0508@gmai.com>
2020-05-19 16:54:41 -0700

    GET requests

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,38 +2,46 @@ import React, { Component } from 'react'
 
 export default class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body: ''
   };
   componentDidMount() {
-    console.log('componentDidMount...');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => this.setState({
+        title: data.title,
+        body: data.body
+      }));
   }
-  componentWillMount() {
-    console.log('componentWillMount...');
-  }
-  componentDidUpdate() {
-    console.log('componentDidUpdate...');
-  }
-  componentWillUpdate() {
-    console.log('componentWillUpdate...');
-  }
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('componentWillReceiveProps...');
-  }
+  // componentWillMount() {
+  //   console.log('componentWillMount...');
+  // }
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate...');
+  // }
+  // componentWillUpdate() {
+  //   console.log('componentWillUpdate...');
+  // }
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('componentWillReceiveProps...');
+  // }
 
-  static getDerivedStateFromError(nextProps, prevState) {
-    return {
-      test: 'something'
-    };
-  }
+  // static getDerivedStateFromError(nextProps, prevState) {
+  //   return {
+  //     test: 'something'
+  //   };
+  // }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUpdate...')
-  }
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapshotBeforeUpdate...')
+  // }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     )
   }
