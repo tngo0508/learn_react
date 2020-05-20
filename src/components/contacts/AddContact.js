3f3d7eec9df32e25cb4d8775502159f3a9cbc6d8
Thomas Ngo <tngo0508@gmai.com>
2020-05-19 17:11:18 -0700

    post & delete requests

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import TextInputGroup from '../layout/TextInputGroup'
-import { v1 as uuid } from 'uuid';
+// import { v1 as uuid } from 'uuid';
+import axios from 'axios';
 
 class AddContact extends Component {
   state = {
@@ -33,14 +34,14 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
+      // id: uuid(),
       name,
       email,
       phone
     }
 
-    dispatch({ type: 'ADD_CONTACT', payload: newContact });
 
+    axios.post('https://jsonplaceholder.typicode.com/users', newContact).then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
 
     // Clear state
     this.setState({
