c9eee81644450cc79da80614d997c3187815cf44
Thomas Ngo <tngo0508@gmai.com>
2020-05-19 18:33:19 -0700

    PUT requests & update action

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -46,6 +46,17 @@ class EditContact extends Component {
       return;
     }
 
+    const updateContact = {
+      name,
+      email,
+      phone,
+    }
+
+    const { id } = this.props.match.params;
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, updateContact);
+
+    dispatch({ type: 'UPDATE_CONTACT', payload: res.data });
+
     // Clear state
     this.setState({
       name: '',
